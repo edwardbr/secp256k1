b812a6f0b4dca7dd82ac54f9cb2411cda44bdc74
Edward Boggis-Rolfe <edward@boggis-rolfe.com>
2019-08-02 02:53:45 +0100

    first bash

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,142 @@
+hunter_config(date VERSION 2.4.1
+    CMAKE_ARGS
+        ANDROID_STL=${ANDROID_STL}
+        ANDROID_ABI=${ANDROID_ABI}
+        ANDROID_PLATFORM=${ANDROID_PLATFORM}
+        CMAKE_ANDROID_ARCH=${CMAKE_ANDROID_ARCH}
+)
+
+hunter_config(Boost VERSION 1.70.0-p0
+    CMAKE_ARGS
+        ANDROID_STL=${ANDROID_STL}
+        ANDROID_ABI=${ANDROID_ABI}
+        ANDROID_PLATFORM=${ANDROID_PLATFORM}
+        CMAKE_ANDROID_ARCH=${CMAKE_ANDROID_ARCH}
+)
+hunter_config(OpenSSL VERSION 1.1.0j
+    CMAKE_ARGS
+        ANDROID_STL=${ANDROID_STL}
+        ANDROID_ABI=${ANDROID_ABI}
+        ANDROID_PLATFORM=${ANDROID_PLATFORM}
+        CMAKE_ANDROID_ARCH=${CMAKE_ANDROID_ARCH}
+)
+hunter_config(Sqlpp11 
+    URL https://github.com/hunter-packages/sqlpp11/archive/v0.57-p0.tar.gz
+    SHA1 472f23489f9a063d2b9a492910ea59a36bc55263
+
+    CMAKE_ARGS
+        ANDROID_STL=${ANDROID_STL}
+        ANDROID_ABI=${ANDROID_ABI}
+        ANDROID_PLATFORM=${ANDROID_PLATFORM}
+        CMAKE_ANDROID_ARCH=${CMAKE_ANDROID_ARCH}
+)
+
+hunter_config(MySQL-client VERSION 6.1.9-p1
+    CMAKE_ARGS
+        ANDROID_STL=${ANDROID_STL}
+        ANDROID_ABI=${ANDROID_ABI}
+        ANDROID_PLATFORM=${ANDROID_PLATFORM}
+        CMAKE_ANDROID_ARCH=${CMAKE_ANDROID_ARCH}
+)
+
+hunter_config(Catch2
+    URL https://github.com/catchorg/Catch2/archive/v2.7.1.tar.gz
+    SHA1 45b3f4ad38f3a5cace6edabd42099de740185237
+    CMAKE_ARGS
+        CATCH_BUILD_TESTING=OFF
+        CATCH_ENABLE_WERROR=OFF
+        CATCH_INSTALL_DOCS=OFF
+        CATCH_INSTALL_HELPERS=ON
+        ANDROID_STL=${ANDROID_STL}
+        ANDROID_ABI=${ANDROID_ABI}
+        ANDROID_PLATFORM=${ANDROID_PLATFORM}
+        CMAKE_ANDROID_ARCH=${CMAKE_ANDROID_ARCH}
+)
+
+hunter_config(sqlcipher
+    URL https://github.com/edwardbr/sqlcipher/archive/hunter-3.27.2.tar.gz
+    SHA1 7c2957823c6e1c16bbe61d08a8b305f00542b66c
+    CMAKE_ARGS
+        ANDROID_STL=${ANDROID_STL}
+        ANDROID_ABI=${ANDROID_ABI}
+        ANDROID_PLATFORM=${ANDROID_PLATFORM}
+        CMAKE_ANDROID_ARCH=${CMAKE_ANDROID_ARCH}
+    )
+
+hunter_config(sqlpp11-connector-mysql
+    URL https://github.com/edwardbr/sqlpp11-connector-mysql/archive/master.tar.gz
+    SHA1 b458ecc6148384810c0cd6e8f0184acdafb0d877
+    CMAKE_ARGS
+        ENABLE_TESTS=Off
+        ANDROID_STL=${ANDROID_STL}
+        ANDROID_ABI=${ANDROID_ABI}
+        ANDROID_PLATFORM=${ANDROID_PLATFORM}
+        CMAKE_ANDROID_ARCH=${CMAKE_ANDROID_ARCH}
+    )
+    
+
+hunter_config(sqlpp11-connector-sqlite3
+    URL https://github.com/edwardbr/sqlpp11-connector-sqlite3/archive/master.tar.gz
+    SHA1 6d408b7cf2dbe60dafa98948a43132d1c65b98ac
+    CMAKE_ARGS
+        SQLCIPHER=On
+        ENABLE_TESTS=Off
+        ANDROID_STL=${ANDROID_STL}
+        ANDROID_ABI=${ANDROID_ABI}
+        ANDROID_PLATFORM=${ANDROID_PLATFORM}
+        CMAKE_ANDROID_ARCH=${CMAKE_ANDROID_ARCH}
+        )
+        
+
+hunter_config(libsodium VERSION 1.0.16-p0
+    CMAKE_ARGS
+        ANDROID_STL=${ANDROID_STL}
+        ANDROID_ABI=${ANDROID_ABI}
+        ANDROID_PLATFORM=${ANDROID_PLATFORM}
+        CMAKE_ANDROID_ARCH=${CMAKE_ANDROID_ARCH}
+)
+hunter_config(flatbuffers VERSION 1.8.0-p1
+    CMAKE_ARGS 
+        FLATBUFFERS_BUILD_TESTS=OFF
+        FLATBUFFERS_BUILD_FLATC=ON
+        FLATBUFFERS_BUILD_FLATHASH=ON
+        ANDROID_STL=${ANDROID_STL}
+        ANDROID_ABI=${ANDROID_ABI}
+        ANDROID_PLATFORM=${ANDROID_PLATFORM}
+        CMAKE_ANDROID_ARCH=${CMAKE_ANDROID_ARCH}
+        )
+
+hunter_config(nlohmann_json VERSION 3.6.1
+CMAKE_ARGS 
+    ANDROID_STL=${ANDROID_STL}
+    ANDROID_ABI=${ANDROID_ABI}
+    ANDROID_PLATFORM=${ANDROID_PLATFORM}
+    CMAKE_ANDROID_ARCH=${CMAKE_ANDROID_ARCH}
+    )
+
+
+hunter_config(nlohmann_fifo_map VERSION 0.0.0-0dfbf5d-p1
+CMAKE_ARGS 
+    ANDROID_STL=${ANDROID_STL}
+    ANDROID_ABI=${ANDROID_ABI}
+    ANDROID_PLATFORM=${ANDROID_PLATFORM}
+    CMAKE_ANDROID_ARCH=${CMAKE_ANDROID_ARCH}
+    )
+    
+
+hunter_config(fmt VERSION 5.2.1
+CMAKE_ARGS 
+    ANDROID_STL=${ANDROID_STL}
+    ANDROID_ABI=${ANDROID_ABI}
+    ANDROID_PLATFORM=${ANDROID_PLATFORM}
+    CMAKE_ANDROID_ARCH=${CMAKE_ANDROID_ARCH}
+    )
+
+
+hunter_config(cxxopts VERSION 2.1.1-pre
+CMAKE_ARGS 
+    ANDROID_STL=${ANDROID_STL}
+    ANDROID_ABI=${ANDROID_ABI}
+    ANDROID_PLATFORM=${ANDROID_PLATFORM}
+    CMAKE_ANDROID_ARCH=${CMAKE_ANDROID_ARCH}
+    )
